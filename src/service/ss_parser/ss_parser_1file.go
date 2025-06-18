689f684f8797dfd78aec09b5eda6bcd357cbd75f
IgorAmi52 <igorami44@gmail.com>
2025-06-18 12:55:42 +0200

    Restructured ss-parser logic

--- src/service/ss_parser/ss_parser_1file.go
+++ src/service/ss_parser/ss_parser_1file.go
@@ -1,13 +1,11 @@
 package ss_parser
 
 import (
-	"encoding/binary"
 	"nosqlEngine/src/models/bloom_filter"
 	"nosqlEngine/src/models/key_value"
 	"nosqlEngine/src/service/file_writer"
 )
 
-const BLOCK_SIZE = 30
 
 type MemValues struct {
 	values []key_value.KeyValue // holds all memtable values that need to be written to SS
@@ -38,7 +36,7 @@ func (ssParser *SSParser1File) parseNextMem() {
 		1. Data section:8 bytes for key size, key, 8 bytes for size of value, value
 		2. Index section: 8 bytes for size of key, key, 8 bytes for offset in data section
 		3. Summary section: 8 bytes for size of key, key, 8 bytes for offset in index section
-		4. MetaData section: 8 bytes summary size, 8 bytes summary start offset,  8 bytes merkle tree size, merkle tree 8 bytes bloom filter size, bloom filter, 8 byters filter size
+		4. MetaData section: 8 bytes for size of summary, 8 bytes for offset of summary in SS file, 8 bytes for size of merkle tree, 8 bytes for size of bloom filter, bloom filter, 8 bytes for number of keys in data section
 
 	*/
 	if ssParser.isParsing {
@@ -58,7 +56,8 @@ func (ssParser *SSParser1File) parseNextMem() {
 	indexBytes, indexOffsets := serializeIndexGetOffsets(keys, keyOffsets, int64(len(dataBytes)))
 	summaryBytes := getSummaryBytes(key_value.GetKeys(data), indexOffsets)
 	summaryOffset := int64(len(dataBytes) + len(indexBytes))
-	metaDataBytes := getMetaDataBytes(int64(len(summaryBytes)), summaryOffset, make([]byte, 0), make([]byte, 0), int64(len(data)))
+	// currently holder 0 bytes for merkle tree and bloom filter
+	metaDataBytes := getMetaDataBytes(int64(len(summaryBytes)), summaryOffset, make([]byte, 0), make([]byte, 0), int64(len(data))) 
 
 	bytes := make([]byte, 0, len(dataBytes)+len(indexBytes)+len(summaryBytes)+len(metaDataBytes))
 	bytes = append(bytes, dataBytes...)
@@ -72,91 +71,4 @@ func (ssParser *SSParser1File) parseNextMem() {
 	} else {
 		ssParser.isParsing = false
 	}
-}
-
-func serializeDataGetOffsets(keyValues []key_value.KeyValue) ([]byte, []string, []int64) {
-
-	currOffset := int64(0)
-	keys := []string{keyValues[0].GetKey()}
-	offsets := []int64{currOffset}
-	dataBytes := []byte{}
-	currBlockSize := 0
-
-	for i := 0; i < len(keyValues); i++ {
-		// key and value blocks
-		value := append(sizeAndValueToBytes(keyValues[i].GetKey()), sizeAndValueToBytes(keyValues[i].GetValue())...)
-
-		if len(value)+currBlockSize > BLOCK_SIZE { // if block is full add to dataBytes
-			if currBlockSize != 0 { // if value is not bigger then the whole block
-				keys = append(keys, keyValues[i].GetKey()) // keyValue| keyValue |keyValue
-				offsets = append(offsets, currOffset)
-				currBlockSize = len(value)
-			}
-			dataBytes = append(dataBytes, value...)
-			currOffset += int64(len(value))
-			continue
-		}
-		dataBytes = append(dataBytes, value...)
-		currOffset += int64(len(value))
-		currBlockSize += len(value)
-	}
-	return dataBytes, keys, offsets
-}
-
-func serializeIndexGetOffsets(keys []string, keyOffsets []int64, startOffset int64) ([]byte, []int64) {
-
-	currOffset := startOffset
-	indexOffsets := []int64{currOffset}
-	dataBytes := make([]byte, 0)
-	currBlockSize := 0
-
-	for i := 0; i < len(keys); i++ {
-		value := append(sizeAndValueToBytes(keys[i]), intToBytes(keyOffsets[i])...)
-		if len(value)+currBlockSize > BLOCK_SIZE { // if block is full add to dataBytes
-			if currBlockSize != 0 { // if value is not bigger then the whole block
-				indexOffsets = append(indexOffsets, currOffset)
-				currBlockSize = len(value)
-			}
-			dataBytes = append(dataBytes, value...)
-			currOffset += int64(len(value))
-			continue
-		}
-		dataBytes = append(dataBytes, value...)
-		currOffset += int64(len(value))
-		currBlockSize += len(value)
-	}
-	return dataBytes, indexOffsets
-}
-func getSummaryBytes(keys []string, offsets []int64) []byte {
-
-	dataBytes := make([]byte, 0)
-	RANGE_SIZE := 20
-
-	for i := 0; i < len(keys); i = i + RANGE_SIZE {
-		value := append(sizeAndValueToBytes(keys[i]), intToBytes(offsets[i])...)
-		dataBytes = append(dataBytes, value...)
-	}
-	return dataBytes
-}
-func getMetaDataBytes(summarySize int64, summaryStartOffset int64, bloomFilterBytes []byte, merkleTreeBytes []byte, dataSize int64) []byte {
-	dataBytes := make([]byte, 0)
-	dataBytes = append(dataBytes, intToBytes(int64(len(bloomFilterBytes)))...)
-	dataBytes = append(dataBytes, bloomFilterBytes...)
-	dataBytes = append(dataBytes, intToBytes(int64(len(merkleTreeBytes)))...)
-	dataBytes = append(dataBytes, merkleTreeBytes...)
-	dataBytes = append(dataBytes, intToBytes(summarySize)...)
-	dataBytes = append(dataBytes, intToBytes(summaryStartOffset)...)
-	dataBytes = append(dataBytes, intToBytes(dataSize)...)
-	return dataBytes
-}
-func intToBytes(n int64) []byte {
-	buf := make([]byte, 8) // 8 bytes for int64
-	binary.BigEndian.PutUint64(buf, uint64(n))
-	return buf
-}
-
-func sizeAndValueToBytes(value string) []byte {
-	valueBytes := []byte(value)
-	valueSizeBytes := intToBytes(int64(len(valueBytes)))
-	return append(valueSizeBytes, valueBytes...)
 }
--- src/service/ss_parser/ss_parser_Mfile.go
+++ src/service/ss_parser/ss_parser_Mfile.go
@@ -31,7 +31,7 @@ func (ssParser *SSParserMfile) parseNextMem() {
 		1. Data section:8 bytes for key size, key, 8 bytes for size of value, value
 		2. Index section: 8 bytes for size of key, key, 8 bytes for offset in data section
 		3. Summary section: 8 bytes for size of key, key, 8 bytes for offset in index section
-		4. MetaData section: 8 bytes summary size, 8 bytes summary start offset,  8 bytes merkle tree size, merkle tree 8 bytes bloom filter size, bloom filter, 8 byters filter size
+		4. MetaData section: 8 bytes for size of summary, 8 bytes for offset of summary in SS file, 8 bytes for size of merkle tree, 8 bytes for size of bloom filter, bloom filter, 8 bytes for number of keys in data section
 		
 	*/
 	if ssParser.isParsing {
