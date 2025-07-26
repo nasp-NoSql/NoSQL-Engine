1ebc3e252e84cc6e83d441c0fa25b5acd642a5b0
kize1509 <businessrepic@gmail.com>
2025-07-26 16:29:14 +0200

    chore: align versions

--- src/engine/write.go
+++ src/engine/write.go
@@ -4,13 +4,11 @@ import (
 	"fmt"
 )
 
-
-func (engine *Engine) Write(user string,key string, value string, fromWal bool) error {
+func (engine *Engine) Write(user string, key string, value string, fromWal bool) error {
 	// check if memory full
 	if engine.checkIfMemtableFull() {
 		return fmt.Errorf("memory is full, cannot write")
 	}
-
 
 	if !fromWal {
 		if ok, err := engine.userLimiter.CheckUserTokens(user); !ok {
@@ -43,16 +41,16 @@ func (engine *Engine) Write(user string,key string, value string, fromWal bool) 
 			defer engine.flush_lock.Unlock()
 
 			engine.ss_parser.FlushMemtable(write_mem.ToRaw())
-			write_mem = 
+			//write_mem =
 			if engine.curr_mem_index == 0 {
 				engine.wal.DeleteWalSegments()
 			}
-			close(done)  // signal that FlushMemtable is done
+			close(done) // signal that FlushMemtable is done
 		}()
 
 		go func() {
-			<-done  // wait for FlushMemtable to finish
-			engine.ss_compacter.CheckCompactionConditions()  
+			<-done // wait for FlushMemtable to finish
+			engine.ss_compacter.CheckCompactionConditions()
 		}()
 	}
 	return nil
